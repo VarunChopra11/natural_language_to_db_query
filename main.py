e3928a2f7d56daceef8b9e4ef29cdc414ea216d7
VarunChopra11 <varunchopra2003@gmail.com>
2025-05-13 01:34:23 +0530

    Remove log level and reload options from Uvicorn run configuration

--- main.py
+++ main.py
@@ -25,4 +25,4 @@ app.include_router(query_routers.router, prefix="/query", tags=["query"])
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="localhost", port=8000, log_level="info", reload=True)
+    uvicorn.run(app, host="localhost", port=8000)
