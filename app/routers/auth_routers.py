080c64e0a3186c7c723279d5f0729c2f892830e4
VarunChopra11 <varunchopra2003@gmail.com>
2025-06-09 21:36:10 +0530

    Update domain for email verification cookie

--- app/routers/auth_routers.py
+++ app/routers/auth_routers.py
@@ -107,7 +107,7 @@ async def verify_email(token: str, pool=Depends(get_connection)):
                 max_age=ACCESS_TOKEN_EXPIRE_MINUTES * 60,
                 secure=True,
                 samesite="none",
-                domain="natural-language-to-db-query.onrender.com"
+                domain="query-nl.vercel.app"
             )
             return response
         
